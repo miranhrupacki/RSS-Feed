1e5ccd540ca31f5a72c18d47c4e4c201c17f30a9
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-25 15:51:57 +0200

    Prepared view which is shown in a list

--- RSSFeed/RSSFeedView/RSSListRowView.swift
+++ RSSFeed/RSSFeedView/RSSListRowView.swift
@@ -1,8 +1,43 @@
 //
-//  RSSListView.swift
+//  RSSListRowView.swift
 //  RSSFeed
 //
 //  Created by Miran Hrupački on 25.09.2024..
 //
 
 import Foundation
+import SwiftUI
+
+struct RSSListRowView: View {
+    var title: String
+    var description: String
+    var imageUrl: String
+
+    var body: some View {
+        HStack(spacing: 20) {
+            Button {
+                print("button tapped")
+            } label: {
+                Image(systemName: "star.fill")
+            }
+            .buttonStyle(PlainButtonStyle())
+            
+            GeometryReader { geometry in
+                Image(imageUrl)
+                    .resizable()
+                    .scaledToFit()
+                    .clipped()
+                    .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
+            }
+            .frame(height: 50)
+            .frame(maxWidth: 50)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(title)
+                    .font(.subheadline)
+                Text(description)
+                    .font(.caption)
+            }
+        }
+    }
+}
