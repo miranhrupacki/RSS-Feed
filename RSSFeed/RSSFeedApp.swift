b63233b4aafe5bd2e84d53d73bade86d2fcc4e8d
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-25 14:57:12 +0200

    Initial Commit

---
+++ RSSFeed/RSSFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSFeedApp.swift
+//  RSSFeed
+//
+//  Created by Miran Hrupački on 25.09.2024..
+//
+
+import SwiftUI
+
+@main
+struct RSSFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
