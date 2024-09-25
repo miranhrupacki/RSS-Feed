e2c59c8081847cd6c55274bd6c461b1a06714b22
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-25 22:15:23 +0200

    setting sink for all publishers, handling call of add, delete functions

--- RSSFeed/RSSFeedView/RSSFeedViewModel.swift
+++ RSSFeed/RSSFeedView/RSSFeedViewModel.swift
@@ -6,15 +6,59 @@
 //
 
 import Foundation
+import FeedKit
+import Combine
 
 class RSSFeedViewModel: ObservableObject {
     @Published var feedProviders: [RSSFeedReponse]
-
+    @Published var rssFeedUrl: String = ""
+    @Published var isSheetPresented: Bool = false
+    @Published var errorMessage: ParserError?
+    @Published var showAlert: Bool
+    
     var feedController: FeedController
+    private var cancellables = Set<AnyCancellable>()
 
     init(feedController: FeedController) {
         self.feedController = feedController
         
         feedProviders = feedController.feedProviders.value
+        errorMessage = feedController.errorMessage.value
+        showAlert = feedController.showAlert.value
+    }
+}
+
+// MARK: - Observers
+private extension RSSFeedViewModel {
+     func setSink() {
+        feedController.feedProviders.sink { [weak self] in
+            self?.feedProviders = $0
+        }.store(in: &cancellables)
+        
+        feedController.showAlert.sink { [weak self] shouldShowAlert in
+            self?.showAlert = shouldShowAlert
+        }.store(in: &cancellables)
+        
+        feedController.errorMessage.sink { [weak self] errorMessage in
+            self?.errorMessage = errorMessage
+        }.store(in: &cancellables)
+    }
+}
+
+// MARK: - Data parser
+extension RSSFeedViewModel {
+    func startFeedKit(with url: String) {
+        feedController.startFeedKit(with: url)
+    }
+}
+
+// MARK: - Update current data
+extension RSSFeedViewModel {
+    func delete(at offsets: IndexSet) {
+        feedController.delete(at: offsets)
+    }
+
+    func addOrUpdateFeed(_ feed: RSSFeedReponse) {
+        feedController.addOrUpdateFeed(feed)
     }
 }
