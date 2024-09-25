ab5a9c73dd6467fc56e6c50cd8dcd43f3bbeb4b8
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-25 22:32:31 +0200

    setup ui, extracted views to extensions for code readability and connecting data, passing data to child views

--- RSSFeed/FavouritesFeedView/FavouritesFeedView.swift
+++ RSSFeed/FavouritesFeedView/FavouritesFeedView.swift
@@ -12,6 +12,45 @@ struct FavouritesFeedView: View {
     @ObservedObject var viewModel: FavouritesFeedViewModel
 
     var body: some View {
-        Text("Favourites")
+        contentView
+    }
+}
+
+// MARK: - Setup UI
+private extension FavouritesFeedView {
+    var contentView: some View {
+        NavigationStack {
+            if viewModel.favouriteFeedProviders.isEmpty {
+                Text("Your favourite list is empty.")
+            } else {
+                listView
+                    .navigationDestination(for: RSSFeedChildItemsViewModel.self, destination: RSSFeedChildItemsView.init)
+                    .navigationTitle("Favourite feeds")
+            }
+        }
+    }
+    
+    var listView: some View {
+        List($viewModel.favouriteFeedProviders, id: \.self) { $item in
+            ForEach($viewModel.favouriteFeedProviders) { $provider in
+                NavigationLink {
+                    RSSFeedChildItemsView(viewModel: RSSFeedChildItemsViewModel(feedTitle: item.title ?? "-", items: item.item))
+                } label: {
+                    RSSListRowView(title: item.title ?? "-",
+                                              description: item.description ?? "-",
+                                              imageUrl: item.imageUrl ?? "-",
+                                              isFavourite: item.isFavourite,
+                                              isFavouriteButtonHidden: false,
+                                              favouriteButtonTapped: { shouldAddToFavourites in
+                        item.isFavourite = shouldAddToFavourites
+                        viewModel.feedController.addOrUpdateFeed(item)
+                    })
+                }
+            }
+            .onDelete(perform: viewModel.delete)
+        }
+        .scrollContentBackground(.hidden)
+        .scrollIndicators(.hidden)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
