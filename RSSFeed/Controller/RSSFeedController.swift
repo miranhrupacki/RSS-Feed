2c28d517f4f7b046c9b2816029f08ac76ca8fafc
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-25 22:14:24 +0200

    Added functions to add/update a feed and delete, handling error message update and calling parser service

--- RSSFeed/Controller/RSSFeedController.swift
+++ RSSFeed/Controller/RSSFeedController.swift
@@ -7,7 +7,48 @@
 
 import Foundation
 import Combine
+import FeedKit
 
 class FeedController {
     var feedProviders: CurrentValueSubject<[RSSFeedReponse], Never> = .init([])
+    var errorMessage: CurrentValueSubject<ParserError?, Never> = .init(nil)
+    var showAlert: CurrentValueSubject<Bool, Never> = .init(false)
+    
+    let feedService = FeedParserService()
+}
+
+// MARK: - Data parser
+extension FeedController {
+    func startFeedKit(with url: String) {
+        feedService.getNewFeed(with: url) { [weak self] result in
+            switch result {
+            case .success(let response):
+                self?.addOrUpdateFeed(response)
+            case .failure(let error):
+                self?.showAlert.value = true
+                self?.errorMessage.value = error
+            }
+        }
+    }
+}
+
+// MARK: - Update current data
+extension FeedController {
+    func addOrUpdateFeed(_ feed: RSSFeedReponse) {
+        if let index = feedProviders.value.firstIndex(where: { $0.id == feed.id }) {
+            feedProviders.value[index] = feed
+        } else {
+            feedProviders.value.append(feed)
+        }
+    }
+    
+    func delete(at offsets: IndexSet) {
+        feedProviders.value.remove(atOffsets: offsets)
+    }
+    
+    func delete(_ feed: RSSFeedReponse) {
+        if let index = feedProviders.value.firstIndex(where: { $0.id == feed.id }) {
+            feedProviders.value.remove(at: index)
+        }
+    }
 }
