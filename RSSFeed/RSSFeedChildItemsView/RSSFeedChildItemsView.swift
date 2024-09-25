6b28c6d1850cefb9c012bbf8524e009139156e1b
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-25 15:50:19 +0200

    Setup List view for data and its row view

--- RSSFeed/RSSFeedChildItemsView/RSSFeedChildItemsView.swift
+++ RSSFeed/RSSFeedChildItemsView/RSSFeedChildItemsView.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RSSFeedChildItemsView: View {
+    var viewModel: RSSFeedChildItemsViewModel
+
+    var body: some View {
+        List(viewModel.items, id: \.self) { item in
+            NavigationLink {
+                Color.red
+            } label: {
+                RSSListRowView(title: item.title ?? "-",
+                               description: item.description ?? "-",
+                               imageUrl: item.imageUrl ?? "-")
+            }
+        }
+        .navigationTitle(viewModel.feedTitle)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollContentBackground(.hidden)
+        .scrollIndicators(.hidden)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.red)
+    }
+}
