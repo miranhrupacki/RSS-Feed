dbb010d4d11450376f50adbd2e72089ba69e6d5b
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-25 15:02:21 +0200

    Created files and groups for views

--- RSSFeed/FavouritesFeedView/FavouritesFeedView.swift
+++ RSSFeed/FavouritesFeedView/FavouritesFeedView.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct FavouritesFeedView: View {
+    var body: some View {
+        Text("Favourites")
+    }
+}
--- RSSFeed/TabBarView/TabBarView.swift
+++ RSSFeed/TabBarView/TabBarView.swift
@@ -5,4 +5,20 @@
 //  Created by Miran Hrupački on 25.09.2024..
 //
 
-import Foundation
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        TabView {
+            RSSFeedView()
+                .tabItem {
+                    Label("Feed", systemImage: "list.dash")
+                }
+            
+            FavouritesFeedView()
+                .tabItem {
+                    Label("Favourites", systemImage: "star")
+                }
+        }
+    }
+}
