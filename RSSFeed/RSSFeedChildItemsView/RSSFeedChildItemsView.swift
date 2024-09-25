9bdb491333d2b2fa85b4650b1850a8629b9bc062
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-25 15:49:08 +0200

    Created views for showing a list of feeds in a rss feed provider, created view models

---
+++ RSSFeed/RSSFeedChildItemsView/RSSFeedChildItemsView.swift
@@ -0,0 +1,8 @@
+//
+//  RSSFeedChildItemsView.swift
+//  RSSFeed
+//
+//  Created by Miran Hrupački on 25.09.2024..
+//
+
+import Foundation
---
+++ RSSFeed/RSSFeedChildItemsView/RSSFeedChildItemsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RSSFeedChildItemsViewModel.swift
+//  RSSFeed
+//
+//  Created by Miran Hrupački on 25.09.2024..
+//
+
+import Foundation
