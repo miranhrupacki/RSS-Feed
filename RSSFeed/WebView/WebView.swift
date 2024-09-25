abdf53a5de6ca6ba8b7aca333ae5345fbd7fa14f
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-25 15:58:48 +0200

    Created webview and passed url to load

--- RSSFeed/WebView/WebView.swift
+++ RSSFeed/WebView/WebView.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+     var urlToLoad: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let url = URL(string: urlToLoad) else { return }
+        uiView.load(URLRequest(url: url))
+    }
+}
