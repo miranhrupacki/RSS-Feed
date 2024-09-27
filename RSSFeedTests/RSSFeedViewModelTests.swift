f46de10933f8d9735f7f087146f6da2294ee81aa
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-27 13:24:27 +0200

    Testing view model for rss feed

--- RSSFeedTests/RSSFeedViewModelTests.swift
+++ RSSFeedTests/RSSFeedViewModelTests.swift
@@ -6,30 +6,85 @@
 //
 
 import XCTest
+import Combine
+import FeedKit
+@testable import RSSFeed
 
 final class RSSFeedViewModelTests: XCTestCase {
+    var feedController: FeedControllerMock!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        feedController = FeedControllerMock()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        feedController = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_startFeedKit() {
+        feedController.startFeedKit(with: "Some test url")
+        XCTAssertTrue(feedController.didCallStartFeedKit)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_favouriteSelected() {
+        let feed = RSSFeedReponse(title: "Test description", description: "Test description", item: [RSSFeedItemReponse()], imageUrl: "Test image url")
+        feedController.favouriteSelected(feed)
+        XCTAssertTrue(feedController.didCallFavouriteSelected)
     }
+    
+    func test_addOrUpdateFeed() {
+        let feed = RSSFeedReponse(title: "Test description", description: "Test description", item: [RSSFeedItemReponse()], imageUrl: "Test image url")
+        feedController.addOrUpdateFeed(feed)
+        XCTAssertTrue(feedController.didCallAddOrUpdate)
+    }
+    
+    func test_delete() {
+        feedController.delete(at: IndexSet(integer: 0))
+        XCTAssertTrue(feedController.didCallDeleteAtOffset)
+    }
+    
+    func test_deleteFeed() {
+        let feed = RSSFeedReponse(title: "Test description", description: "Test description", item: [RSSFeedItemReponse()], imageUrl: "Test image url")
+        feedController.delete(feed)
+        XCTAssertTrue(feedController.didCallDeleteFeed)
+    }
+}
 
+// Since our ViewModels are pretty simple, and there is not much going on, tests are not complicated.
+// Basically ViewModels are unnecessary when we have a FeedController.
+// ViewModels are here just for a code separation.
+
+class FeedControllerMock: FeedControllerProtocol {
+    var feedProviders: CurrentValueSubject<[RSSFeedReponse], Never> = .init([])
+    var errorMessage: CurrentValueSubject<ParserError?, Never> = .init(nil)
+    var showAlert: CurrentValueSubject<Bool, Never> = .init(false)
+    var feedService = FeedParserService()
+    
+    var didCallStartFeedKit: Bool = false
+    var didCallFavouriteSelected: Bool = false
+    var didCallAddOrUpdate: Bool = false
+    var didCallDeleteAtOffset: Bool = false
+    var didCallDeleteFeed: Bool = false
+
+    func startFeedKit(with url: String) {
+        didCallStartFeedKit = true
+    }
+    
+    func favouriteSelected(_ feed: RSSFeedReponse) {
+        didCallFavouriteSelected = true
+    }
+    
+    func addOrUpdateFeed(_ feed: RSSFeedReponse) {
+        didCallAddOrUpdate = true
+    }
+    
+    func delete(at offsets: IndexSet) {
+        didCallDeleteAtOffset = true
+    }
+    
+    func delete(_ feed: RSSFeedReponse) {
+        didCallDeleteFeed = true
+    }
 }
