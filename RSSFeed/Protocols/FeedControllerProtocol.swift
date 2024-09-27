407fd796dbc7b8908810e6ee7cadbaa56402106e
Miran Hrupački <miran.hrupacki@barrage.net>
2024-09-27 13:23:09 +0200

    Created protocol for feed controller for testing purposes

--- RSSFeed/Protocols/FeedControllerProtocol.swift
+++ RSSFeed/Protocols/FeedControllerProtocol.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import Combine
+import FeedKit
+
+protocol FeedControllerProtocol {
+    var feedProviders: CurrentValueSubject<[RSSFeedReponse], Never> { get set }
+    var errorMessage: CurrentValueSubject<ParserError?, Never> { get set }
+    var showAlert: CurrentValueSubject<Bool, Never> { get set }
+    var feedService: FeedParserService { get set }
+    
+    func startFeedKit(with url: String)
+    func favouriteSelected(_ feed: RSSFeedReponse)
+    func addOrUpdateFeed(_ feed: RSSFeedReponse)
+    func delete(at offsets: IndexSet)
+    func delete(_ feed: RSSFeedReponse)
+}
